f81cc1815b49a87d83d6351a688846dbe42452e0
hayet <habethayet@gmail.com>
2017-09-12 11:18:24 +0200

    processeur

---
+++ index.ts
@@ -0,0 +1,6 @@
+import {Compteur} from './compteur';
+const compteur = new Compteur();
+compteur.increment();
+compteur.increment();
+compteur.increment();
+console.log(compteur.showCount());
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: {
+        index: './index.ts'
+    },
+    output: {
+        filename: './dist/[name].bundle.js'
+
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devtool: 'inline-source-map'
+}
